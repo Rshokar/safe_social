27a8ea77c29e112d69edf766af9dd5c4f6cafe01
Rshokar <ravindershokar@hotmail.com>
2021-03-22 18:15:06 -0700

    Integrate logout.js scripts with on_run.js

--- scripts/on_run.js
+++ scripts/on_run.js
@@ -1,29 +1,21 @@
-let restricted = ["account.html", "exposures.html", "bookingconfirm.html", "booking.html"]
+let restricted = ["account.html", "exposures.html", "bookingconfirm.html", "booking.html", "events.html"]
 
 console.log(firebase)
-var userName = "test";
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log(user)
         console.log('User logged in')
         console.log("Page location is " + window.location.href);
-
+    } else {
         let url = window.location.href
         url = url.split("/")
 
         let page = url[url.length - 1]
-
+        console.log(page)
         for (i = 0; i < restricted.length; i++) {
-            console.log()
             if (page == restricted[i]) {
-                window.location.replace("http://127.0.0.1:5500/")
+                window.location.replace("http://127.0.0.1:5500/login.html")
             }
         }
-
-    } else {
-        let url = window.location.href
-        url = url.split("/")
-        console.log(url[url.length - 1])
-        console.log('User not logged in')
     }
 });
