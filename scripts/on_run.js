0ffde45d0380b536fb753a6597df390a87989abd
Rshokar <ravindershokar@hotmail.com>
2021-03-22 17:58:20 -0700

    Made ajdustments to on_run.js

--- scripts/on_run.js
+++ scripts/on_run.js
@@ -1,9 +1,28 @@
+let restricted = ["account.html", "exposures.html", "bookingconfirm.html", "booking.html"]
+
 console.log(firebase)
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log(user)
         console.log('User logged in')
+        console.log("Page location is " + window.location.href);
+
+        let url = window.location.href
+        url = url.split("/")
+
+        let page = url[url.length - 1]
+
+        for (i = 0; i < restricted.length; i++) {
+            console.log()
+            if (page == restricted[i]) {
+                window.location.replace("http://127.0.0.1:5500/")
+            }
+        }
+
     } else {
+        let url = window.location.href
+        url = url.split("/")
+        console.log(url[url.length - 1])
         console.log('User not logged in')
     }
 });
