ce8cfa86aa639583ccaa0ed604c90db64a046503
Rshokar <ravindershokar@hotmail.com>
2021-03-23 19:36:59 -0700

    I created the account.js file and made a dynamic account page. The account page will automatically adjust the name and be able to update account information. The user inputs still need to be cleaned and varified

--- scripts/on_run.js
+++ scripts/on_run.js
@@ -1,21 +1,19 @@
 let restricted = ["account.html", "exposures.html", "bookingconfirm.html", "booking.html", "events.html"]
 
-console.log(firebase)
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        console.log(user)
-        console.log('User logged in')
-        console.log("Page location is " + window.location.href);
+        console.log("user logged in")
+        console.log(user.displayName)
     } else {
         let url = window.location.href
         url = url.split("/")
 
         let page = url[url.length - 1]
-        console.log(page)
         for (i = 0; i < restricted.length; i++) {
-            if (page == restricted[i]) {
+            if (page.includes(restricted[i])) {
                 window.location.replace("http://127.0.0.1:5500/login.html")
             }
         }
     }
 });
+
