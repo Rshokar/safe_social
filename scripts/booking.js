23c2c7f0812a619dd7b99fa59ca4ef840ce6389d
Rshokar <ravindershokar@hotmail.com>
2021-03-26 10:24:36 -0700

    worked on UI for booking and booking confirm.

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,21 +1,17 @@
 function setDataBooking() {
     var myEvent = document.getElementById("example-text-input").value;
-    var myEmail = document.getElementById("example-email-input").value;
-    var myPhone = document.getElementById("example-tel-input").value;
     var myLocation = document.getElementById("example-password-input").value;
     var myDate = document.getElementById("example-date-input").value;
     var myTime = document.getElementById("example-time-input").value;
 
     var obj = {
         "event": myEvent,
-        "email": myEmail,
-        "phone": myPhone,
         "location": myLocation,
         "date": myDate,
         "time": myTime
     }
-    console.log(obj);
     localStorage.setItem('formdata', JSON.stringify(obj));
 }
 
 setDataBooking();
+
