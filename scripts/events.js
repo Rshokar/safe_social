73b768af8940790881009638a491ece624c1e0f1
Brendan Lin <blin63@my.bcit.ca>
2021-03-31 14:44:58 -0700

    made some fixes to bookingconfirm and booking and some mods to the events.js

--- scripts/events.js
+++ scripts/events.js
@@ -1,24 +1,29 @@
-
+//this function prints the events that the user is part of to the html page
 function readEvent() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    //console.log(user.uid);
+    var id = user.uid;
+    let tableData = [];
+    var entryCounter = 0;
 
-    var id = firebase.auth().onAuthStateChanged(function (user) {
-      console.log(user.uid);
+    db.collection("Event").where("UID", "==", id)
+      .get()
+      .then(function (snap) {
+        snap.forEach(function (doc) {
+          $(document).ready(function () {
 
-      var id = user.uid;
-      let tableData = [];
+            var location = doc.data().Location;
+            var date = doc.data().Date;
 
-      db.collection("Event").where("UID", "==", id)
-        .get()
-        .then(function (snap) {
-          snap.forEach(function (doc) {
-            $(document).ready(function () {
+            //console.log(location);
+            //console.log(date);
+            //console.log(snap.size);
+            //if a entry is found in database, increment counter and print data to page
+            if (snap.size > 0) {
+              entryCounter++;
 
-              var location = doc.data().Location;
-              var date = doc.data().Date;
-              console.log(location);
-              console.log(date);
-
-                tableData.push($("<a href='#' class='list-group-item list-group-item-action'>" +
+              //adds the html containing the event data to the tableData array
+              tableData.push($("<a href='#' class='list-group-item list-group-item-action'>" +
                 "<div class='d-flex w-100 justify-content-between'>" +
                 "<h5 class='mb-1'>" + location + "</h5>" +
                 "<small class='text-muted'>A few seconds ago</small>" +
@@ -26,11 +31,24 @@ function readEvent() {
                 "<small class='text-muted'>" + date + "</small>" +
                 "</a>"));
 
+              //append tableData to html page
               $("#content").append(tableData);
-            });
-          })
+            }
+          });
         })
-    });
-  }
+      })
+  });
 
-  readEvent();
+  //check if there are any entries
+  checkNumberOfEntries(entryCounter);
+}
+
+readEvent();
+
+//this function checks if there are any events that the user is part of in the database
+function checkNumberOfEntries(entries) {
+  if (entries == 0) {
+    let msg = $("<h1>There are no events created yet</h1>");
+    $("#content").append(msg);
+  }
+}
