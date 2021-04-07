48c02fd088534a7d08eae4160d1b640db0211ddf
Rshokar <ravindershokar@hotmail.com>
2021-04-07 15:16:13 -0700

    Fixed bug where navbar was disappearing on account.html page.

--- scripts/account.js
+++ scripts/account.js
@@ -45,7 +45,6 @@ $(document).ready(function () {
         let email = $("#inputEmail").val()
         let name = $("#inputName").val()
 
-
         user.updateProfile({
             displayName: name
         }).then(function () {
@@ -123,6 +122,9 @@ $('#freinds_search_bar').on('input', function () {
  * They do this by changing the CSS display setting and the color of the tabs.   
  */
 function cleanPages() {
+
+    $('#freinds_list').empty();
+
     $('.tab').css({
         'background-color': '#EE964b',
         'color': 'white'
