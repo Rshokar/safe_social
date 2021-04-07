da75a69c94d5caf7499541aa22f5adcc83d79b9b
Brendan Lin <blin63@my.bcit.ca>
2021-04-06 21:50:04 -0700

    writeExposure() modified to get event details and event of report to exposure collection

--- scripts/exposure.js
+++ scripts/exposure.js
@@ -1,27 +1,76 @@
 //function to write exposure report to database
 function writeExposure() {
+    //user variables
     var id;
-    var time;
+
+    //time string
+    var timeFrame = firebase.firestore.Timestamp.now().toDate();
+    var restriction = new Date(timeFrame - 12096e5);
+    var yyyy = restriction.getFullYear();
+    var mm = restriction.getMonth() + 1;
+    var dd = restriction.getDate();
+    var dateString = yyyy + "-" + mm + "-" + dd;
+    var reportedDate = document.getElementById("exposure").value;
+    //console.log(reportedDate);
+    //event variables
+    var event;
+    var eventDate;
+    var location;
+    var guest;
+    var hostId;
+
+    //if month is less than october (10), add a 0 to make it in dd format
+    if (mm < 10) {
+        dateString = yyyy + "-" + "0" + mm + "-" + dd;
+    }
+    //console.log(dateString);
 
     firebase.auth().onAuthStateChanged(function (user) {
-        var exposureRef = db.collection("Exposure");
-        id = user.uid;
-        time = firebase.firestore.Timestamp.now().toDate().toString();
 
-        exposureRef.add({
-            Event: document.getElementById("exposure").value,
-            UID: id,
-            Time: time
-        }).then(function() {
-            window.location.reload();
-        });
+        id = user.uid;
+        //console.log(id);
+
+        db.collection("Event")
+            .where("date", ">=", dateString)
+            .where("host.id", "==", id)
+            .get()
+            .then(function (snap) {
+                snap.forEach(function (doc) {
+                    //console.log(doc.data().event);
+                    //console.log(doc.data().guest);
+                    event = doc.data().event;
+                    eventDate = doc.data().date;
+                    location = doc.data().location;
+                    guest = doc.data().guest;
+                    hostId = doc.data().host.id;
+                    //console.log(hostId);
+                });
+            }).then(function () {
+                //call the exposure collection to write to it
+                var exposureRef = db.collection("Exposure");
+
+                exposureRef.add({
+                    reportedEvent: event,
+                    eventDate: eventDate,
+                    location: location,
+                    guest: guest,
+                    hostId: hostId,
+                    reportedDate: reportedDate
+                }).then(function () {
+                    window.location.reload();
+                });
+            });
+
+
     })
 }
+
+//writeExposure();
 
 //console.log(document.getElementById("exposure").value);
 
 //function to read the exposure reports from database
-function readExposure() {
+/*function readExposure() {
     firebase.auth().onAuthStateChanged(function (user) {
         var id = user.uid;
         let tableData = [];
@@ -54,3 +103,4 @@ function readExposure() {
 }
 
 readExposure();
+*/
