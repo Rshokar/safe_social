926b08fddd4fa0d4d175bf8cf46b62e86f38be0b
Brendan Lin <blin63@my.bcit.ca>
2021-04-08 10:24:18 -0700

    modified exposure.js to post all events within a 2 week window from all events that user is a host or a guest at an event

--- scripts/exposure.js
+++ scripts/exposure.js
@@ -1,17 +1,19 @@
 //function to write exposure report to database
-function writeExposure() {
+function writeExposureHost() {
     //user variables
     var id;
 
     //time string
+    var reportedDate = document.getElementById("exposure").value;
     var timeFrame = firebase.firestore.Timestamp.now().toDate();
-    var restriction = new Date(timeFrame - 12096e5);
+    var convert = new Date(reportedDate);
+    var restriction = new Date(convert - 12096e5);
     var yyyy = restriction.getFullYear();
     var mm = restriction.getMonth() + 1;
     var dd = restriction.getDate();
     var dateString = yyyy + "-" + mm + "-" + dd;
-    var reportedDate = document.getElementById("exposure").value;
-    //console.log(reportedDate);
+    //console.log(restriction);
+    //console.log(timeFrame);
     //event variables
     var event;
     var eventDate;
@@ -44,24 +46,21 @@ function writeExposure() {
                     guest = doc.data().guest;
                     hostId = doc.data().host.id;
                     //console.log(hostId);
+                    //call the exposure collection to write to it
+                    var exposureRef = db.collection("Exposure");
+
+                    exposureRef.add({
+                        reportedEvent: event,
+                        eventDate: eventDate,
+                        location: location,
+                        guest: guest,
+                        hostId: hostId,
+                        reportedDate: reportedDate
+                    });
                 });
             }).then(function () {
-                //call the exposure collection to write to it
-                var exposureRef = db.collection("Exposure");
-
-                exposureRef.add({
-                    reportedEvent: event,
-                    eventDate: eventDate,
-                    location: location,
-                    guest: guest,
-                    hostId: hostId,
-                    reportedDate: reportedDate
-                }).then(function () {
-                    window.location.reload();
-                });
+                writeExposureGuest();
             });
-
-
     })
 }
 
@@ -76,13 +75,15 @@ function writeExposureGuest() {
     var username;
 
     //time string
-    var timeFrame = firebase.firestore.Timestamp.now().toDate();
-    var restriction = new Date(timeFrame - 12096e5);
+    //var timeFrame = firebase.firestore.Timestamp.now().toDate();
+    var reportedDate = document.getElementById("exposure").value;
+    var convert = new Date(reportedDate);
+    var restriction = new Date(convert - 12096e5);
     var yyyy = restriction.getFullYear();
     var mm = restriction.getMonth() + 1;
     var dd = restriction.getDate();
     var dateString = yyyy + "-" + mm + "-" + dd;
-    var reportedDate = document.getElementById("exposure").value;
+    
     //console.log(reportedDate);
     //event variables
     var event;
@@ -106,34 +107,32 @@ function writeExposureGuest() {
         //console.log(username);
 
         db.collection("Event")
-            //.where("guest." + id + ".name", "==", username)
-            .where("date", ">=", dateString)
+            .where("guest." + id + ".name", "==", username)
             .get()
             .then(function (snap) {
                 snap.forEach(function (doc) {
-                    console.log(doc.data().event);
-                    //console.log(doc.data().guest);
-                    /*event = doc.data().event;
-                    eventDate = doc.data().date;
-                    location = doc.data().location;
-                    guest = doc.data().guest;
-                    hostId = doc.data().host.id;
-                    //console.log(hostId); */
+                    //console.log(doc.data().event);
+                    if (doc.data().date >= dateString) {
+                        console.log(doc.data().event);
+                        event = doc.data().event;
+                        eventDate = doc.data().date;
+                        location = doc.data().location;
+                        guest = doc.data().guest;
+                        hostId = doc.data().host.id;
+
+                        //call the exposure collection to write to it
+                        var exposureRef = db.collection("Exposure");
+
+                        exposureRef.add({
+                            reportedEvent: event,
+                            eventDate: eventDate,
+                            location: location,
+                            guest: guest,
+                            hostId: hostId,
+                            reportedDate: reportedDate
+                        });
+                    }
                 });
-            }).then(function () {
-                //call the exposure collection to write to it
-                /*var exposureRef = db.collection("Exposure");
-                
-                exposureRef.add({
-                    reportedEvent: event,
-                    eventDate: eventDate,
-                    location: location,
-                    guest: guest,
-                    hostId: hostId,
-                    reportedDate: reportedDate
-                }).then(function () {
-                    window.location.reload();
-                }); */
             });
     })
 }
