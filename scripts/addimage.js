73f360c9e42b6a677ccbe5bee085e85da0267fcb
TrevorHong <trevorhong159@gmail.com>
2021-04-13 16:04:50 -0700

    Changes

--- scripts/addimage.js
+++ scripts/addimage.js
@@ -1,47 +1,17 @@
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+firebase.initializeApp(config);
+var storage = firebase.storage();
+var storageRef = storage.ref();
+var tangRef = storageRef.child('images/8009894933_898f81b5ec_b.jpg');
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
+// First we sign in the user anonymously
+firebase.auth().signInAnonymously().then(function() {
+  // Once the sign in completed, we get the download URL of the image
+  tangRef.getDownloadURL().then(function(url)                             {
+    // Once we have the download URL, we set it to our img element
+    document.querySelector('img').src = url;
+    
+  }).catch(function(error) {
+    // If anything goes wrong while getting the download URL, log the error
+    console.error(error);
+  });
+});
