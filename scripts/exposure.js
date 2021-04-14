acdc25b89924b0885c9653fd2d9c27b021bb0309
Brendan Lin <blin63@my.bcit.ca>
2021-04-14 16:26:27 -0700

    fixed exposure reload

--- scripts/exposure.js
+++ scripts/exposure.js
@@ -49,14 +49,24 @@ function writeExposureHost() {
                     //call the exposure collection to write to it
                     var exposureRef = db.collection("Exposure");
 
-                    exposureRef.add({
-                        reportedEvent: event,
-                        eventDate: eventDate,
-                        location: location,
-                        guest: guest,
-                        hostId: hostId,
-                        reportedDate: reportedDate
-                    });
+                    if (guest != undefined) {
+                        exposureRef.add({
+                            reportedEvent: event,
+                            eventDate: eventDate,
+                            location: location,
+                            guest: guest,
+                            hostId: hostId,
+                            reportedDate: reportedDate
+                        });
+                    } else {
+                        exposureRef.add({
+                            reportedEvent: event,
+                            eventDate: eventDate,
+                            location: location,
+                            hostId: hostId,
+                            reportedDate: reportedDate
+                        });
+                    }    
                 });
             }).then(function () {
                 writeExposureGuest();
@@ -123,14 +133,24 @@ function writeExposureGuest() {
                         //call the exposure collection to write to it
                         var exposureRef = db.collection("Exposure");
 
-                        exposureRef.add({
-                            reportedEvent: event,
-                            eventDate: eventDate,
-                            location: location,
-                            guest: guest,
-                            hostId: hostId,
-                            reportedDate: reportedDate
-                        });
+                        if (guest != undefined) {
+                            exposureRef.add({
+                                reportedEvent: event,
+                                eventDate: eventDate,
+                                location: location,
+                                guest: guest,
+                                hostId: hostId,
+                                reportedDate: reportedDate
+                            });
+                        } else {
+                            exposureRef.add({
+                                reportedEvent: event,
+                                eventDate: eventDate,
+                                location: location,
+                                hostId: hostId,
+                                reportedDate: reportedDate
+                            });
+                        }    
                     }
                 });
             }).then(setTimeout(function () {
