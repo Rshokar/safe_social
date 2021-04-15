6b3fb88c9f89f9ca6edda41904e3ebad6fd8dbe5
Rshokar <ravindershokar@hotmail.com>
2021-04-15 13:33:18 -0700

    Fixed a bug where guest could not see events they have been invited to. Needed to adjust how db is quering gues t. Made changes to evet_details.html and events.js. Additionaly i made minor stylinch changes to main.css. I only changes the color of the text in the mobile nav bar.

--- scripts/events.js
+++ scripts/events.js
@@ -50,53 +50,44 @@ $(document).ready(function () {
         })
       });
 
+    /**
+     * This query looks for all events in which current user is a guest. Then 
+     * uses createevent() located in create_event_block.js. 
+     * @author Ravidner Shokar
+     * @version 1.0    
+     */
     db.collectionGroup('guest')
       .where("name", '==', username)
       .get()
       .then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
-          console.log(doc.ref.path);
-          console.log(doc.ref.parent.parent.id)
-          db.collection('Event')
-            .doc(doc.ref.parent.parent.id)
-            .get()
-            .then((doc) => {
-              if (doc.exists) {
-                console.log("Document data:", doc.data());
-                console.log("Document data:", doc.id);
-                var tableData = [];
-                tableData.push(doc.data().event, doc.data().date, doc.data().location, doc.id);
-                $("#guest_events").append(createEvent(tableData));
-                console.log("Document data:", doc.id);
-                eventDetailsListner(doc.id);
+          console.log("Document data:", doc.data());
+          if (doc.exists) {
+            console.log(doc.ref.path);
+            console.log(doc.ref.parent.parent.id)
+            db.collection('Event')
+              .doc(doc.ref.parent.parent.id)
+              .get()
+              .then((doc) => {
+                if (doc.exists) {
+                  console.log("Document data:", doc.data());
+                  console.log("Document data:", doc.id);
+                  var tableData = [];
+                  tableData.push(doc.data().event, doc.data().date, doc.data().location, doc.id);
+                  $("#guest_events").append(createEvent(tableData));
+                  console.log("Document data:", doc.id);
+                  eventDetailsListner(doc.id);
 
-              } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-              }
-            })
+                } else {
+                  console.log("No such document!");
+                }
+              })
+          } else {
+            $("#guest_events").append("<h1>There are no events created yet</h1>")
+          }
+
         });
       })
-    /**
-    var tableData2 = [];
-    tableData2.push(doc.data().event, doc.data().date, doc.data().location, doc.id);
-    //console.log(tableData2);
-    //console.log(createEvent(tableData2));
-    $("#host_events").append(createEvent(tableData2));
-    eventDetailsListner(doc.id);
-    /*tableData2.push($(`
-      <div id='${doc.id}' href='#' class='list-group-item list-group-item-action'>
-        <div class='d-flex w-100 justify-content-between'>
-          <h5 class='mb-1'>${doc.data().event}</h5>
-          <small class='text-muted'></small>
-        </div>
-        <small class='text-muted date'>${doc.data().date}</small></br>
-        <small class='text-muted location'>${doc.data().location}</small>
-      </div>"));
-      `))
-    //append tableData2 to html
-    $("#guest_events").append(tableData2);
-    eventDetailsListner(doc.id); */
 
   });
 
@@ -111,7 +102,13 @@ $(document).ready(function () {
   });
 })
 
-
+/**
+ * This event listener redirects user to event_details.html. Adds event ID 
+ * to url. 
+ * @author Ravinder Shokar
+ * @version 1.0
+ * @param id is the ID of the event
+ */
 function eventDetailsListner(id) {
   document.getElementById(id)
     .addEventListener('click', function () {
